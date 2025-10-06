b8ae57c3657ed42e4c0a366a9d2871d33c703ef9
Rifushigi <rifushigi.teda@gmail.com>
2025-10-06 17:32:01 +0100

    feat: add Placeholder record to represent variable placeholders in document templates

---
+++ app/src/main/java/org/tessera/domain/model/Placeholder.java
@@ -0,0 +1,20 @@
+package org.tessera.domain.model;
+
+/**
+ * Represents a variable placeholder found within a document template.
+ *
+ * @param varName The name of the variable, e.g "fullName".
+ * @param fullText The full text of the placeholder including delimiters, e.g ${fullName}.
+ * */
+public record Placeholder(String varName, String fullText) {
+
+    public Placeholder{
+        if (varName == null || varName.isBlank()){
+            throw new IllegalArgumentException("Variable name cannot be blank or null");
+        }
+
+        if (fullText == null || fullText.isBlank()){
+            throw new IllegalArgumentException("Full text cannot be null or blank.");
+        }
+    }
+}
